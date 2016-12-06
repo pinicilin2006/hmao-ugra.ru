f3fb738977559f9ec94b899bccbf23b0451bdc72
pinicilin2006 <pinicilin2006@ya.ru>
2016-12-06 21:40:43 +0500

    Update message.php

--- message.php
+++ message.php
@@ -10,7 +10,7 @@ if(!$name || !$name2 || !$name3 || !$personal_data){
 	echo 'Все поля должны быть заполнены';
 	exit();
 }
-$to      = 'slava@hmao-ugra.ru';
+$to      = 'slava@hmao-ugra.ru,slava1@hmao-ugra.ru';
 $subject = 'Заявка на порубочный билет';
 $message = 'Заявка на порубочный билет с сайта: Имя:'.$name.'. Фамилия'.$name2.'. Отчество:'.$name3;
 $headers = 'From: slava@hmao-ugra.ru' . "\r\n" .
